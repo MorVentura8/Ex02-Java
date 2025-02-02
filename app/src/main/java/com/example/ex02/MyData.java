f84a59873b605a562c8ab0b7d222399289115a47
Segevc98 <156806005+Segevc98@users.noreply.github.com>
2025-02-03 00:33:35 +0200

    support for checkout and cart

---
+++ app/src/main/java/com/example/ex02/MyData.java
@@ -0,0 +1,14 @@
+package com.example.ex02;
+
+public class MyData {
+
+    public static String[] nameProduct = {"PlatStation 5", "Dualshock Controller",
+            "Xbox", "Xbox Controller","Gaming computer", "FC 25",
+            "Call Of Duty Black Ops 6", "Spider-Man 2"};
+
+    public static Integer[] drawbleArray = {R.drawable.loginphoto,R.drawable.loginphoto,R.drawable.loginphoto,R.drawable.loginphoto,R.drawable.loginphoto,R.drawable.loginphoto,R.drawable.loginphoto,R.drawable.loginphoto};
+
+    public static String[] priceArray = { "1600" ,"300" , "1600","300","7000","250","250","199" };
+
+    public static Integer[] id_ = {0,1,2,3,4,5,6,7};
+}
